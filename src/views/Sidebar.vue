16bd2bf33c6c4ed23d8bdee4b186c33e41ac34fd
13949 <1394949540@qq.com>
2021-03-12 10:41:08 +0800

    commit

---
+++ src/views/Sidebar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link to="/Example01">Welcome</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/views/example01/Example01.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>Welcome</div>
+    {{ msg }}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data: () => ({
+    msg: "hello world"
+  })
+});
+</script>
